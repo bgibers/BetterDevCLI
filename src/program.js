e29f2d1bcda6773ef53587064f99cd786a5744ba
Brendan Giberson <bgibers@clemson.edu>
2018-12-27 15:20:46 -0500

    Needed YAML modules added

--- src/program.js
+++ src/program.js
@@ -1,3 +1,19 @@
-const readYaml = require('js-yaml');
-const url = "https://raw.githubusercontent.com/yeo/betterdev.link/master/content/issues/1.yml"
+//Imports
+const yaml = require('yaml');
 
+var latestIssue = 1
+const url = "https://raw.githubusercontent.com/yeo/betterdev.link/master/content/issues/" + latestIssue + ".yml"
+
+/*
+    Scrape web using url
+    Create function to check date and do new scrape
+    Save different links to a db
+    Convert yaml to js-yaml for more functionality 
+    modulaize functions
+    create functions to retrieve from db
+ */
+
+//scrape web here and store as string
+var text = ``
+
+console.log(yaml.parse(text))

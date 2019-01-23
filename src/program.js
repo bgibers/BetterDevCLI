978406431273576e5a2293a65d1be73bea415d5c
Brendan Giberson <bgibers@clemson.edu>
2019-01-22 22:17:57 -0500

    Everything scraped correctly

--- src/program.js
+++ src/program.js
@@ -1,26 +1,9 @@
 const rp = require('request-promise');
-const $ = require('cheerio');
 const puppeteer = require('puppeteer');
 const url = 'https://betterdev.link/issues/1';
 const articleTitles = [];
 const articleUrls = [];
 
-// rp(url)
-//   .then(function(html){
-//     //success!
-//         const linkCount = $('.issue-link > a', html).length;
-//         const articleTitles = [];
-//         const articleUrls = [];
-
-//         for(let i = 0; i < linkCount; i++) {
-//             articleUrls.push($('.issue-link > a', html)[i].attribs.href);
-//         }
-//         console.log(articleTitles);
-//         console.log(articleUrls);
-//     })
-//   .catch(function(err){
-//     //handle error
-//   });
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -34,9 +17,14 @@ const articleUrls = [];
         let container = document.querySelectorAll('div.issue-link');
 
         for (var element of container) {
-            articleTitles.push(element.querySelector('a').innerText);
-            articleUrls.push(element.querySelector('a').href);
-            articlePars.push(element.querySelectorAll('p')[1].textContent);
+            try {
+                articleTitles.push(element.querySelector('a').innerText);
+                articleUrls.push(element.querySelector('a').href);
+                articlePars.push(element.querySelectorAll('p')[1].innerHTML);
+            } catch(e) {
+                // statements
+                 console.log(e);
+            }   
         }
         return {articleTitles,articleUrls,articlePars}
     }); 
